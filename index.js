95513be11996436adc21ef2cc0888d634834e32f
ayush-khatrii <khatriayush2003@gmail.com>
2026-01-02 15:49:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import nodeCron from "node-cron";
+import dotenv from "dotenv";
+import { publishBlogs, publishNews } from "./worker/worker.js";
+
+
+const  formatTime = (time) => {
+  return {
+    hours: time.getHours(),
+    minutes: time.getMinutes(),
+    seconds: time.getSeconds(),
+  }
+}
+dotenv.config();
+
+const app = express();
+const port = 5000;
+
+nodeCron.schedule("* * * * *", async () => {
+  console.log("⏰ Cron running:", new Date().toLocaleString());
+
+  try {
+    const result = await publishBlogs();
+    console.log("✅ Cron success:", result);
+  } catch (error) {
+    console.error("❌ Cron error:", error.message);
+  }
+});
+
+nodeCron.schedule("* * * * *", async () => {
+  console.log(" Cron running:", new Date().toLocaleString());
+
+  try {
+    const newsCron = await publishNews();
+    const blogCron = await publishBlogs();
+
+    console.log("✅ Cron News Resp::", newsCron);
+    console.log("✅ Cron Blog Resp::", blogCron);
+  } catch (error) {
+    console.error("❌ Cron error:", error.message);
+  }
+});
+
+// health check
+app.get("/", (req, res) => {
+  res.send("Cron worker running");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Cron server running on http://localhost:${port}`);
+});
---
+++ worker/worker.js
@@ -0,0 +1,33 @@
+const apiUrl = process.env.API_BASE_URL;
+
+export async function publishBlogs() {
+  const res = await fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.CRON_JOB_SECRET}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Cron failed: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
+}
+
+export async function publishNews() {
+  const res = await fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.CRON_JOB_SECRET}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Cron failed: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
+}
